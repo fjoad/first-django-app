fa4afa7ecc697cc1338ce54c291e6251c1c2ff46
fjoad <fjoad@andrew.cmu.edu>
2020-09-08 02:14:35 +0300

    created better views using templates and linked using url namespacing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+#from django.http import Http404
+#from django.template import loader
+
+from .models import Question 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #template = loader.get_template('polls/index.html')
+    #return HttpResponse("Hello World. You're at the polls index.")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(reqeust, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
